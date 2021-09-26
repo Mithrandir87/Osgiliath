b7c68275f90d9d1e4b0b01429e7055ab4b16c7b8
Mithrandir87 <sliferdrago@gmail.com>
2021-09-26 11:53:54 +0200

    Updated projects

--- projects/views.py
+++ projects/views.py
@@ -10,5 +10,4 @@ def projects(request):
 
 def project(request, pk):
     projectObj = Project.objects.get(id=pk)
-    tags = projectObj.tags.all()
-    return render(request, 'projects/single-project.html', {'projectObj' : projectObj, 'tags' : tags})
+    return render(request, 'projects/single-project.html', {'projectObj' : projectObj})
